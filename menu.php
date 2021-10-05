9ad9c4d39bffbb85d24f4e9ee68b5a2744af47b4
Naim Jhuboo <naim.jhuboo@gmail.com>
2021-10-05 16:05:53 +0200

    Matrix

--- menu.php
+++ menu.php
@@ -1,27 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="menu.css" />
-    <script src="menu.js" defer></script>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="menu.css" />
+  <script src="menu.js" defer></script>
 </head>
+
 <body>
   <nav class="navbar">
     <div class="navbar-links">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </div>
-    <div class="brand-title">Neo Name</div>
-    <a href="#" class="toggle-button">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
+      <ul>
+        <li id="home_menu"><a href="#"><img src="Ressources/home.png" class="img_menu">
+            <p>Home</p>
+          </a></li>
+        <li id="about"><a href="#"><img src="Ressources/user.png" class="img_menu">
+            <p>About</p>
+          </a></li>
+        <li id="skills"><a href="#"><img src="Ressources/document.png" class="img_menu">
+            <p>Skills</p>
+          </a></li>
+        <li id="services"><a href="#"><img src="Ressources/case (1).png" class="img_menu">
+            <p>Services</p>
+          </a></li>
+        <li id="portfolio"><a href="#"><img src="Ressources/landscape.png" class="img_menu">
+            <p>Portfolio</p>
+          </a></li>
+        <li id="contact"><a href="#"><img src="Ressources/send.png" class="img_menu">
+            <p>Contact me</p>
+          </a></li>
+      </ul>
+    </div>
+    <div class="flex_nav">
+    <div class="brand-title">Keanue Reeves</div>
+    <a href="matrix/matrix.html" class="matrix_mode"><img src="Ressources/icons8-néo.svg" id="neo_img"></a>
+    <a class="toggle-button">
+      <span class="bar" id="line1"></span>
+      <span class="bar" id="line2"></span>
+      <span class="bar" id="line3"></span>
     </a>
+    </div>
   </nav>
 </body>
+
 </html>
