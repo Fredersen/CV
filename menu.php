847386ec01f475a5789f9f356eb571a073a9aa84
Naim Jhuboo <naim.jhuboo@gmail.com>
2021-10-05 18:22:56 +0200

    des petits changements

--- menu.php
+++ menu.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="menu.css" />
-  <script src="menu.js" defer></script>
-</head>
-
-<body>
   <nav class="navbar">
     <div class="navbar-links">
       <ul>
@@ -43,6 +31,3 @@
     </a>
     </div>
   </nav>
-</body>
-
-</html>
--- skills.php
+++ skills.php
@@ -1,19 +1,4 @@
-<!--============ Skills ============== -->
-<!--==== section compétences=====-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Skills</title>
-    <link rel="stylesheet" type="text/css" href="skills.css"/>
-    <link rel="stylesheet" type="text/css" href="styles.css"/>
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
-</head>
-
-<body>
     <section class = "skills section" id="skills">
         <h2 class = "section_title">Compétences</h2>
         <span class = "section_subtitle">Mon niveau technique </span>
@@ -245,6 +230,3 @@
         </div>
     </div>
     </section>
-    <script type="text/JavaScript" src="skills.js"></script>
-</body>
-</html>
