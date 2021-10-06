d6ce7f8d0afd90535fa9faea202f7c7c44571925
Naim Jhuboo <naim.jhuboo@gmail.com>
2021-10-06 11:44:06 +0200

    j'ai rajouté plein de truc

---
+++ CV-main/contact.php
@@ -0,0 +1,95 @@
+
+<section class="contact section" id="contact">
+        <h2 class="section_title">Contact</h2>
+        <span class="section_subtitle">Get in Touch</span>
+
+        <div class="contact_container container grid"> 
+            <div>
+
+        <div class="contact_information">
+            <i class="fas fa-phone-alt"></i>
+        
+        <div>
+            <h3 class="contact_title">Call Me</h3>
+            <span class="contact_subtitle">555-123-456</span>
+        </div>
+    </div>
+
+    <div class="contact_container container grid"> 
+        <div>
+
+    <div class="contact_information">
+        <i class="fas fa-at"></i>
+    
+    <div>
+        <h3 class="contact_title">Email</h3>
+        <span class="contact_subtitle">Thomas@Anderson.com</span>
+    </div>
+</div>
+     </div>
+        </div>
+        <div class="contact_container container grid"> 
+            <div>
+    
+        <div class="contact_information">
+            <i class="fas fa-map-marker-alt"></i>
+    
+        <div>
+            <h3 class="contact_title">Location</h3>
+            <span class="contact_subtitle"> 123 Matrix street, Los Angeles CA 92210</span>
+        </div>
+    </div>
+</div>
+</div>
+
+<form class="contact_form"><br>
+    <div class="contact_content">
+        <label for="firstname" class="contact_label">First Name</label><br>
+        <input type="text" id="firstname" class="contact_input" name="fname"><br>
+        <label for="lastname" class="contact_label">Last Name</label><br>
+        <input type="text" id="lastname" class="contact_input" name="lname"><br>
+        <label for="email" class="contact_label">Email</label><br>
+        <input type="email" id="email" class="contact_input" name="email"><br>
+        <label for="message">Message</label><br>
+        <textarea id="message" name="message" class="message"></textarea>
+        <br><br>  
+        <i class="fas fa-paper-plane"></i>
+        <input type="submit" value="Send Message">
+      
+    </div>
+</form>
+</section>
+
+<!--==================== FOOTER =====================-->
+<section class="footer">
+    <footer>
+    <div class="footer_bg">
+        <div class="footer_container container grid">
+            <div>
+                <h3 class="footer_title">Thomas Anderson</h3>
+                <span class="footer_subtitle">Web Developer</span>
+            </div>
+
+            <div class="footer_socials">
+                <a href="https://www.facebook.com/" target="_blank" class="footer_socials">
+                    <i class="fab fa-facebook-f"></i>
+                    
+                </a>
+                
+                <a href="https://www.github.com/" target="_blank" class="footer_socials">
+                    <i class="fab fa-github"></i>
+                    
+                </a>
+                <a href="https://www.linkedin.com/" target="_blank" class="footer_socials">
+                    <i class="fab fa-linkedin"></i>
+                    
+                </a>
+            </div>
+        </div>
+
+        <p class="footer_copy">&#169; Wilders CV. copyrights</p>
+    </div>
+    </footer>
+</section>
+<link rel="stylesheet" href="contact.js">
+
